52051bc6f193a14258cb5df27357d62cdcd24ab4
Christo Wilken <christo.wilken@googlemail.com>
2025-01-10 14:31:26 +0100

    refactor: Improve code formatting and readability in instantiate.py

--- promptwizard/glue/promptopt/instantiate.py
+++ promptwizard/glue/promptopt/instantiate.py
@@ -30,13 +30,15 @@ class GluePromptOpt:
         PREDICTED_ANS = "predicted_ans"
         LLM_OUTPUT = "llm_output"
 
-    def __init__(self,
-                 prompt_config_path: str,
-                 setup_config_path: str,
-                 dataset_jsonl: str,
-                 data_processor: DatasetSpecificProcessing,
-                 dataset_processor_pkl_path: str = None,
-                 prompt_pool_path: str = None):
+    def __init__(
+            self,
+            prompt_config_path: str,
+            setup_config_path: str,
+            dataset_jsonl: str,
+            data_processor: DatasetSpecificProcessing,
+            dataset_processor_pkl_path: str = None,
+            prompt_pool_path: str = None
+            ):
         """
         Collates all the configs present in different yaml files. Initialize logger, de-serialize pickle file that has
         class/method for dataset processing (for given dataset).
@@ -60,15 +62,18 @@ class GluePromptOpt:
 
         prompt_config_dict = yaml_to_dict(prompt_config_path)
         prompt_opt_cls, prompt_opt_hyperparam_cls, promptpool_cls = get_promptopt_class(
-            prompt_config_dict[PromptOptimizationLiterals.PROMPT_TECHNIQUE_NAME])
+            prompt_config_dict[PromptOptimizationLiterals.PROMPT_TECHNIQUE_NAME]
+        )
 
         self.setup_config = yaml_to_class(setup_config_path, SetupConfig)
         self.prompt_opt_param = yaml_to_class(prompt_config_path, prompt_opt_hyperparam_cls)
         current_dir = dirname(__file__)
-        default_yaml_path = join(current_dir,
-                                 "techniques",
-                                 prompt_config_dict[PromptOptimizationLiterals.PROMPT_TECHNIQUE_NAME],
-                                 "prompt_pool.yaml")
+        default_yaml_path = join(
+            current_dir,
+            "techniques",
+            prompt_config_dict[PromptOptimizationLiterals.PROMPT_TECHNIQUE_NAME],
+            "prompt_pool.yaml"
+            )
 
         self.prompt_pool = yaml_to_class(prompt_pool_path, promptpool_cls, default_yaml_path)
 
@@ -76,22 +81,28 @@ class GluePromptOpt:
             dataset = read_jsonl(dataset_jsonl)
         self.prompt_opt_param.answer_format += self.prompt_pool.ans_delimiter_instruction
         base_path = join(self.setup_config.dir_info.base_dir, self.setup_config.experiment_name)
-        set_logging_config(join(base_path, self.setup_config.dir_info.log_dir_name),
-                           self.setup_config.mode)
+        set_logging_config(
+            join(base_path, self.setup_config.dir_info.log_dir_name),
+            self.setup_config.mode
+            )
         self.logger = get_glue_logger(__name__)
 
         if dataset_jsonl != None:
             if len(dataset) < self.prompt_opt_param.seen_set_size:
                 self.prompt_opt_param.seen_set_size = len(dataset)
-                self.logger.info(f"Dataset has {len(dataset)} samples. However values for seen_set_size is "
-                                f"{self.prompt_opt_param.seen_set_size}. Hence resetting seen_set_size"
-                                f" to {len(dataset)}")
+                self.logger.info(
+                    f"Dataset has {len(dataset)} samples. However values for seen_set_size is "
+                    f"{self.prompt_opt_param.seen_set_size}. Hence resetting seen_set_size"
+                    f" to {len(dataset)}"
+                    )
 
         if self.prompt_opt_param.few_shot_count > self.prompt_opt_param.seen_set_size:
             self.prompt_opt_param.few_shot_count = self.prompt_opt_param.seen_set_size
-            self.logger.info(f"Value set for few_shot_count is {self.prompt_opt_param.few_shot_count}. "
-                             f"However values for seen_set_size is {self.prompt_opt_param.seen_set_size}. "
-                             f"Hence resetting few_shot_count to {self.prompt_opt_param.few_shot_count}")
+            self.logger.info(
+                f"Value set for few_shot_count is {self.prompt_opt_param.few_shot_count}. "
+                f"However values for seen_set_size is {self.prompt_opt_param.seen_set_size}. "
+                f"Hence resetting few_shot_count to {self.prompt_opt_param.few_shot_count}"
+                )
 
         if dataset_jsonl != None:
             training_dataset = dataset[:self.prompt_opt_param.seen_set_size]
@@ -103,10 +114,14 @@ class GluePromptOpt:
         # This iolog is going to be used when doing complete evaluation over test-dataset
         self.iolog.reset_eval_glue(join(base_path, "evaluation"))
 
-        self.prompt_opt = prompt_opt_cls(training_dataset, base_path, self.setup_config,
-                                         self.prompt_pool, self.data_processor, self.logger)
+        self.prompt_opt = prompt_opt_cls(
+            training_dataset, base_path, self.setup_config,
+            self.prompt_pool, self.data_processor, self.logger
+            )
 
-    def get_best_prompt(self,use_examples=False,run_without_train_examples=False,generate_synthetic_examples=False) -> (str, Any):
+    def get_best_prompt(
+            self, use_examples=False, run_without_train_examples=False, generate_synthetic_examples=False
+            ) -> (str, Any):
         """
         Call get_best_prompt() method of class PromptOptimizer & return its value.
         :return: (best_prompt, expert_profile)
@@ -115,7 +130,10 @@ class GluePromptOpt:
             identity of described in expert_profile.
         """
         start_time = time.time()
-        self.BEST_PROMPT, self.EXPERT_PROFILE = self.prompt_opt.get_best_prompt(self.prompt_opt_param,use_examples=use_examples,run_without_train_examples=run_without_train_examples,generate_synthetic_examples=generate_synthetic_examples)
+        self.BEST_PROMPT, self.EXPERT_PROFILE = self.prompt_opt.get_best_prompt(
+            self.prompt_opt_param, use_examples=use_examples, run_without_train_examples=run_without_train_examples,
+            generate_synthetic_examples=generate_synthetic_examples
+            )
 
         self.logger.info(f"Time taken to find best prompt: {(time.time() - start_time)} sec")
         return self.BEST_PROMPT, self.EXPERT_PROFILE
@@ -131,19 +149,23 @@ class GluePromptOpt:
         start_time = time.time()
         self.logger.info(f"Evaluation started {CommonLogsStr.LOG_SEPERATOR}")
         if not self.BEST_PROMPT:
-            self.logger.error("BEST_PROMPT attribute is not set. Please set self.BEST_PROMPT attribute of this object, "
-                              "either manually or by calling get_best_prompt() method.")
+            self.logger.error(
+                "BEST_PROMPT attribute is not set. Please set self.BEST_PROMPT attribute of this object, "
+                "either manually or by calling get_best_prompt() method."
+                )
             return
 
         total_correct = 0
         total_count = 0
         for json_obj in read_jsonl_row(test_dataset_jsonl):
-            answer = self.predict_and_access(json_obj[DatasetSpecificProcessing.QUESTION_LITERAL],
-                                             json_obj[DatasetSpecificProcessing.FINAL_ANSWER_LITERAL])
-      
+            answer = self.predict_and_access(
+                json_obj[DatasetSpecificProcessing.QUESTION_LITERAL],
+                json_obj[DatasetSpecificProcessing.FINAL_ANSWER_LITERAL]
+                )
+
             total_correct += answer[self.EvalLiterals.IS_CORRECT]
             total_count += 1
-            result = {"accuracy": f"{total_correct}/{total_count} : {total_correct/total_count}%",
+            result = {"accuracy": f"{total_correct}/{total_count} : {total_correct / total_count}%",
                       "predicted": answer[self.EvalLiterals.PREDICTED_ANS],
                       "actual": json_obj[DatasetSpecificProcessing.FINAL_ANSWER_LITERAL]}
             self.iolog.append_dict_to_chained_logs(result)
@@ -167,12 +189,13 @@ class GluePromptOpt:
                 llm_output -> Output text generated by LLM for the given question
         :rtype: (bool, str, str)
         """
-        final_prompt = self.prompt_pool.eval_prompt.format(instruction=self.BEST_PROMPT,
-                                                           question=question)
+        final_prompt = self.prompt_pool.eval_prompt.format(
+            instruction=self.BEST_PROMPT,
+            question=question
+            )
         llm_output = self.prompt_opt.chat_completion(user_prompt=final_prompt, system_prompt=self.EXPERT_PROFILE)
-        
+
         is_correct, predicted_ans = self.data_processor.access_answer(llm_output, gt_answer)
         return {self.EvalLiterals.IS_CORRECT: is_correct,
                 self.EvalLiterals.PREDICTED_ANS: predicted_ans,
                 self.EvalLiterals.LLM_OUTPUT: llm_output}
-
